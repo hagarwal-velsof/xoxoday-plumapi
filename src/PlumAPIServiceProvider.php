f99153255010105a51015290730e71c92ac840a3
Harsh <hagarwal@velsof.com>
2022-10-28 11:24:35 +0530

    'removed'

--- src/PlumAPIServiceProvider.php
+++ src/PlumAPIServiceProvider.php
@@ -24,7 +24,6 @@ class PlumAPIServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
     }
 }
